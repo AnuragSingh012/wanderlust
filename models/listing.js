7091419ca0b1870424e7be5fe7b20c860b2aedab
Anurag Singh <anuragsingh4932@gmail.com>
2024-01-09 15:50:12 +0530

    Add Footer

--- models/listing.js
+++ models/listing.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Review = require("./review.js");
 
 const listingSchema = new mongoose.Schema({
     title: {
@@ -31,6 +32,12 @@ const listingSchema = new mongoose.Schema({
     }
 });
 
-const Listing = mongoose.model('Listing', listingSchema);
+listingSchema.post("findOneAndDelete", async (listing) => {
+    if(listing) {
+        await Review.deleteMany({ _id : {$in: listing.reviews} });
+    }
+});
 
+
+const Listing = mongoose.model('Listing', listingSchema);
 module.exports = Listing;
