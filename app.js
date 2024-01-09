d74f16288f6b198401481080bf522a6db3967779
Anurag Singh <anuragsingh4932@gmail.com>
2024-01-09 21:04:55 +0530

    Add New Feature

--- app.js
+++ app.js
@@ -13,13 +13,16 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 const session = require("express-session");
+const MongoStore = require('connect-mongo');
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 const flash = require("connect-flash");
 
+const dbUrl = process.env.ATLASDB_URL;
+
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+    await mongoose.connect(dbUrl);
 }
 
 main()
@@ -37,8 +40,20 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto: {
+        secret: process.env.SECRET
+    },
+    touchAfter: 24 * 3600,
+});
+
+store.on("error", () => {
+    console.log("ERROR in MONGO SESSION STORE", err);
+});
 
 const sessionOptions = {
+    store,
     secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
@@ -48,6 +63,8 @@ const sessionOptions = {
         httpOnly: true,
     },
 };
+
+
 
 app.use(session(sessionOptions));
 app.use(flash());
